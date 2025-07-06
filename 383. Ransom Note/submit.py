d18026c80f3ecf0dfa5577ada496be7caa5151af
daedaluschan <daedalus.chan@gmail.com>
2025-07-06 16:32:56 +0100

    first draft 383

--- 383. Ransom Note/submit.py
+++ 383. Ransom Note/submit.py
@@ -1,5 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         for note_letter in ransomNote:
-            for mag_letter in magazine:
+            found = False
+            for idx, mag_letter in enumerate(magazine):
                 if note_letter == mag_letter:
+                    found = True
+                    mag_letter[idx] = None
+
+            if not found:
+                return False
+
+        return True
