636a10ffc9959ca77ce52cee68decce7bffc47a7
daedaluschan <daedalus.chan@gmail.com>
2025-07-07 23:25:36 +0100

    even number of digits

--- 05.Array/Find Numbers with Even Number of Digits/Submit.py
+++ 05.Array/Find Numbers with Even Number of Digits/Submit.py
@@ -1,1 +1,19 @@
 
+class Solution:
+    def is_even_digit(self, num):
+        digit = 1
+        quotient = num
+        while quotient >= 10:
+            quotient = quotient/10
+            digit += 1
+        return (digit % 2 == 0)
+
+    def findNumbers(self, nums: List[int]) -> int:
+        even_digit_num = 0
+        for num in nums:
+            if self.is_even_digit(num):
+                even_digit_num += 1
+        return even_digit_num
+        
+# Time complexity: O(n)
+# Space complexity: O(1)
